8bf1299faf5eeecba8804aaf80c399ef1bd4d34d
Chad Allen <chadevanallen@Gmail.com>
2023-06-13 22:03:02 -0500

    basic structuring with links to future pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default function FirstPost() {
-    return <h1>Hello World!</h1>
+import Link from "next/link"
+
+
+export default function Home() {
+    return (
+    <div>
+        <h1>Hello World!</h1>
+        <h2>
+            <Link href='pages/about'>About Me</Link>
+            <br />
+            <Link href='pages/contact'>Contact Information</Link>
+            <br />
+            <Link href='pages/porfolio'>My Work</Link>
+        </h2>
+    </div>
+    )
 }
+
+
